8fd8ab822d09778f25c5b8c09148c02e0e8e0130
Brian S. O'Neill <bronee@gmail.com>
2025-04-26 10:42:07 -0700

    Add basic validation test.

---
+++ agent/src/test/java/org/cojen/boxtin/ValidationTest.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2025 Cojen.org
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cojen.boxtin;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * 
+ *
+ * @author Brian S. O'Neill
+ */
+public class ValidationTest {
+    public static void main(String[] args) throws Exception {
+        org.junit.runner.JUnitCore.main(ValidationTest.class.getName());
+    }
+
+    @Test
+    public void java_base() throws Exception {
+        new RulesBuilder().applyRules(RulesApplier.java_base()).validate(ModuleLayer.boot());
+    }
+}
