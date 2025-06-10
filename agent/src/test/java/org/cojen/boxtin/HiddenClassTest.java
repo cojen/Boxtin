a0418137aa10728e2176e74ea28c17665c3386b7
Brian S. O'Neill <bronee@gmail.com>
2025-06-09 17:00:30 -0700

    Add a test to verify that hidden classes cannot be created.

---
+++ agent/src/test/java/org/cojen/boxtin/HiddenClassTest.java
@@ -0,0 +1,62 @@
+/*
+ *  Copyright 2025 Cojen.org
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cojen.boxtin;
+
+import java.lang.invoke.MethodHandles;
+
+import org.cojen.maker.ClassMaker;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Defining hidden classes must be denied, because otherwise it breaks how the current caller
+ * class is obtained.
+ *
+ * @author Brian S. O'Neill
+ */
+public class HiddenClassTest extends TransformTest {
+    public static void main(String[] args) throws Exception {
+        org.junit.runner.JUnitCore.main(HiddenClassTest.class.getName());
+    }
+
+    private static final Rules RULES = 
+        new RulesBuilder().applyRules(RulesApplier.java_base()).build();
+
+    @Test
+    public void basic() throws Exception {
+        if (runWith(RULES)) {
+            return;
+        }
+
+        byte[] bytes = ClassMaker.begin().finishBytes();
+
+        var lookup = MethodHandles.lookup();
+
+        try {
+            lookup.defineHiddenClass(bytes, false);
+            fail();
+        } catch (SecurityException e) {
+        }
+
+        try {
+            lookup.defineHiddenClassWithClassData(bytes, "data", false);
+            fail();
+        } catch (SecurityException e) {
+        }
+    }
+}
